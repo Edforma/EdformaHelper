2e9c690ee4ab0480a0cf13eb8a3ded6327471c63
Griffin <griffin@edforma.app>
2024-07-30 12:02:19 -0500

    blah blah

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -13,12 +13,12 @@ module.exports = {
     production : {
       user : 'edforma',
       host : '45.76.232.86',
-      ref  : 'origin/v2',
+      ref  : 'origin/main',
       repo : 'https://github.com/Edforma/EdformaHelper.git',
       path : '/home/edforma/pm2Data/EdformaHelper',
-      'pre-setup': "pwd && ls -la && uname -a && bun --version",
+      'pre-setup': "pwd && ls -la && uname -a && /home/edforma/.bun/bin/bun --version",
       "pre-deploy-local": "echo Uploading ecosystem... && scp ecosystem.config.cjs edforma@45.76.232.86:'/home/edforma/' && echo Ecosystem uploaded.",
-      'post-deploy' : 'yarn && pm2 reload ~/ecosystem.config.cjs --env production',
+      'post-deploy' : 'bun install && pm2 reload ~/ecosystem.config.cjs --env production',
       "env": {
         "SENTRY_DSN": "https://dcf35fe725fc46babcfeace2f5dd3db2@o1069103.ingest.sentry.io/6065463",
         "SENTRY_TRACESAMPLERATE": 0.5,
